5fae21884ae79a62b477b2e9e218771ea6ccd09a
joshuabhogal <>
2023-06-09 10:20:05 +0100

    Changed from the regular JSX table to one that looks more government style Spacing has been added between each job to make each one easier to read

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import Table from '@govuk-react/table';
 import React, {useEffect, useState} from "react"
 import axios from "axios";
 import QRCode from "qrcode"
+import {GridCol, GridRow} from "govuk-react";
 
 const MainPage = () => {
     const [jobs, setJobs] = useState(null)
@@ -55,26 +56,25 @@ function getWindowSize(){
     if (typeof window !== "undefined"){
         const windowWidth = window.innerWidth
         const windowHeight = window.innerHeight
-
+        console.log(windowWidth)
+        console.log(windowHeight)
+        // 1633 x 910 comfortably fits 4 jobs no problem
         switch (windowWidth){
-            case 2560 : return 6;   // 2560 x ~~~~
-                break;
-            case 1920 : return 4;   // 1920 x ~~~~
-                break;
-            case 1440 : return 11;  // 1440 x ~~~~
-                break;
+            case 2560 : return 5;   // 2560 x ~~~~
+            case 1920 : return 3;   // 1920 x ~~~~
+            case 1440 : return 9;  // 1440 x ~~~~
             case 1080 : return 5;   // 1080 x ~~~~
-                break;
-            default: return 4;
-                break;
+            default: return 3;
         }
     }
 }
 
-// Height, Width
-// 1329, 2560 ----- 969, 1920   landscape
+// Height(normal/fullscreen), Width
+// 1304/1329, 2560 ----- 969, 1920   landscape
 // 2449, 1440 ----- 1809, 1080  portrait
 //console.log(window.innerHeight, window.innerWidth)
+
+// anything above 2000px wide comfortably fits 6 jobs (needs 1300px min height)
 function tableCreate2(responseData){
 
     let size = getWindowSize()
@@ -83,36 +83,59 @@ function tableCreate2(responseData){
     if (responseData === null){
         return "";
     }
+
     for (let i = 0; i < size; i++){
 
         let jobItem = responseData[i];
 
-        const dataRow = <tr>
-            <td>{jobItem.title}</td>
-            <td>{jobItem.summary.substring(0,300) + "... Please scan the QR Code for more information"}</td>
-            <td>{jobItem.company}</td>
-            <td>{jobItem.location.location}</td>
-            <td className={"qrcode"}><img src = {jobItem.qrCode}/></td>
-        </tr>;
+        // const dataRow = <tr>
+        //     <td>{jobItem.title}</td>
+        //     <td>{jobItem.summary.substring(0,300) + "... Please scan the QR Code for more information"}</td>
+        //     <td>{jobItem.company}</td>
+        //     <td>{jobItem.location.location}</td>
+        //     <td><img src = {jobItem.qrCode}/></td>
+        // </tr>;
+        // jobs.push(dataRow)
+
+        // const dataRow = <GridRow>
+        //     <GridCol setWidth={"10%"}>{jobItem.title}</GridCol>
+        //     <GridCol setWidth={"50%"}>{jobItem.summary.substring(0,300) + "... Please scan the QR Code for more information"}</GridCol>
+        //     <GridCol setWidth={"10%"}>{jobItem.company}</GridCol>
+        //     <GridCol setWidth={"10%"}>{jobItem.location.location}</GridCol>
+        //     <GridCol><img src = {jobItem.qrCode}/></GridCol>
+        // </GridRow>;
+        // jobs.push(dataRow)
+
+        const dataRow = <Table>
+
+            <Table.Row>
+                <Table.Cell>
+                    {jobItem.title}
+                </Table.Cell>
+                <Table.Cell>
+                    {jobItem.summary.substring(0,300) + "Please scan the QR Codes for more information"}
+                </Table.Cell>
+                <Table.Cell>
+                    {jobItem.company}
+                </Table.Cell>
+                <Table.Cell>
+                    {jobItem.location.location}
+                </Table.Cell>
+                <Table.Cell>
+                    <img src = {jobItem.qrCode}/>
+                </Table.Cell>
+            </Table.Row>
+        </Table>
         jobs.push(dataRow)
+
+
     }
 
-    return (
-        <table className={"jobCardTable"}>
-            <caption>This is all a first design of a digital Job Board - Made by Josh Bhogal (I'm a T-level student, Feedback is appreciated)</caption>
-            <tbody>
-            <tr>
-                <th>Job Title</th>
-                <th>Job Description</th>
-                <th>Company</th>
-                <th>Job Location</th>
-                <th>QR Code</th>
-            </tr>
-            {jobs}
-            </tbody>
-        </table>
 
-        // <Table className={"jobCardTable"}>
+
+
+    return (
+        // <table className={"jobCardTable"}>
         //     <caption>This is all a first design of a digital Job Board - Made by Josh Bhogal (I'm a T-level student, Feedback is appreciated)</caption>
         //     <tbody>
         //     <tr>
@@ -124,7 +147,18 @@ function tableCreate2(responseData){
         //     </tr>
         //     {jobs}
         //     </tbody>
-        // </Table>
+        // </table>
+
+        <Table className={"jobCardTable"} caption={"This is a test for a digital Job Board made by Josh Bhogal (I'm a T-Level student, Feedback is appreciated)"}
+               // head={<Table.Row><Table.CellHeader>Job Title</Table.CellHeader>
+               //     <Table.CellHeader>Job Description</Table.CellHeader>
+               //     <Table.CellHeader>Company</Table.CellHeader>
+               //     <Table.CellHeader>Location</Table.CellHeader>
+               //     <Table.CellHeader>QR Code</Table.CellHeader>
+               // </Table.Row>}
+        >
+            {jobs}
+        </Table>
 
     )
 }
