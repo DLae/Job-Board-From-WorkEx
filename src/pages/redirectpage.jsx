dc77babe702f305c1432d4bcb6801248e44bbc40
joshuabhogal <>
2023-06-16 06:58:01 +0100

    Using the second page as a placeholder in order for a backend to be implemented at a later date, should also redirect the user to the job website  for the qr code that they scanned

--- src/pages/redirectpage.jsx
+++ src/pages/redirectpage.jsx
@@ -1,14 +1,16 @@
 import "../app/globals.css";
 import "../app/gov.scss";
+import * as querystring from "querystring";
 
 const redirectpage = () =>{
+    const urlParams = new URLSearchParams(querystring)
+    const redirectLink = urlParams.get("redirecturl")
+    let newPageOpen;
+    if (typeof window !== "undefined") {
+        newPageOpen = window.open(redirectLink, "_blank")
+    }
 
-
-    return(
-        <h1>This should work now i think</h1>
-    )
-
-
+    return(newPageOpen)
 }
 
 
