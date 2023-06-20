3b1533913953524c7c6f89eab821a9834ab847ac
joshuabhogal <>
2023-06-20 16:26:07 +0100

    Redirect works perfectly

--- src/pages/redirectpage.jsx
+++ src/pages/redirectpage.jsx
@@ -1,13 +1,11 @@
 import "../app/globals.css";
 import "../app/gov.scss";
 
-const redirectpage = () =>{
-    let jobWindow;
+const redirectpage = () => {
     if (typeof window !== "undefined") {
-        jobWindow = window.open("about:blank", "_blank")
         const indexOfH = window.location.search.indexOf("h")
         const searchQuery = window.location.search.substring(indexOfH, window.location.search.length)
-        jobWindow.location.href = `${searchQuery}`
+        window.location.replace(searchQuery)
     }
 
     return null
