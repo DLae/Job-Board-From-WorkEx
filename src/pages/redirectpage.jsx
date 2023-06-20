24782edcb5616a1294d2f094d56945589eb87f6e
joshuabhogal <>
2023-06-20 12:34:18 +0100

    Reverted to part of the code where the QR is now just the job link for the tv in QH

--- src/pages/redirectpage.jsx
+++ src/pages/redirectpage.jsx
@@ -2,17 +2,15 @@ import "../app/globals.css";
 import "../app/gov.scss";
 
 const redirectpage = () =>{
-    let newPageOpen;
+    let jobWindow;
     if (typeof window !== "undefined") {
+        jobWindow = window.open("about:blank", "_blank")
         const indexOfH = window.location.search.indexOf("h")
         const searchQuery = window.location.search.substring(indexOfH, window.location.search.length)
-
-        newPageOpen = window.open("about:blank", '_blank', searchQuery)
+        jobWindow.location.href = `${searchQuery}`
     }
 
-    return(newPageOpen)
+    return null
 }
-
-
 
 export default redirectpage
