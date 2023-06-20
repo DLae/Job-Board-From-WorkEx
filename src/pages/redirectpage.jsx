bd29fbe7fc266dc789ebff23c789f97cfd3ad4a1
joshuabhogal <>
2023-06-20 09:48:32 +0100

    Added some code that should get around the popup blocker

--- src/pages/redirectpage.jsx
+++ src/pages/redirectpage.jsx
@@ -4,10 +4,11 @@ import "../app/gov.scss";
 const redirectpage = () =>{
     let newPageOpen;
     if (typeof window !== "undefined") {
+        newPageOpen = window.open("/jobshowcase", '_blank')
         const indexOfEquals = window.location.search.indexOf("=")
         const searchQuery = window.location.search.substring(indexOfEquals + 1, window.location.search.length)
 
-        newPageOpen = window.open(searchQuery)
+        newPageOpen.location.href = `${searchQuery}`
     }
 
     return(newPageOpen)
